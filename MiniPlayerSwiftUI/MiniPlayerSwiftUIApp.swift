a6e2132465e6f1cb5e14f28ddbfcf858bc96ccf8
Aziz Bibitov <azizbibitov11@gmail.com>
2026-06-17 13:43:08 +0500

    Initial Commit

---
+++ MiniPlayerSwiftUI/MiniPlayerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniPlayerSwiftUIApp.swift
+//  MiniPlayerSwiftUI
+//
+//  Created by Aziz Bibitov on 17.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct MiniPlayerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
